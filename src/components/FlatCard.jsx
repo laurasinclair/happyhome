5e2c693a1283991582d687f001606569bb60d264
Laura Sinclair <l.sinclair@tuta.io>
2024-04-21 19:20:16 +0200

    media query mixins work!

--- src/components/FlatCard.jsx
+++ src/components/FlatCard.jsx
@@ -1,7 +1,7 @@
 import { Trash, StarFill } from 'react-bootstrap-icons'
 import placeholder from '@img/placeholder_image.jpg'
 import Button from '@components/Button'
-import styles from './FlatCard.module.sass';
+import styles from './FlatCard.module.sass'
 
 export default function FlatCard(props) {
 	const { rental, clickToDelete } = props
@@ -13,33 +13,38 @@ export default function FlatCard(props) {
 	return (
 		<div className={styles.flatcard}>
 			<div className={styles.flatcard_thumbnail} style={{ backgroundImage: 'url(' + placeholder + ')' }}>
-				{rental.review_scores_rating && <p className={styles.flatcard_score}><StarFill size="15" /><span>{(rental.review_scores_rating / 20).toFixed(1)}</span>/5</p>}
+				{rental.review_scores_rating && (
+					<p className={styles.flatcard_score}>
+						<StarFill size="15" />
+						<span>{(rental.review_scores_rating / 20).toFixed(1)}</span>/5
+					</p>
+				)}
 			</div>
 			<div className={styles.flatcard_body}>
-				<div>
-					<h3>{rental.name}</h3>
-					<p>
-						<strong>ID:</strong> {rental.id}
-					</p>
-					<p>
-						<strong>Country:</strong> {rental.country}
-					</p>
-					<p>
-						<strong>City:</strong> {rental.city}
-						{rental.neighbourhood ? ' 📍 ' + rental.neighbourhood : null}
-					</p>
-					<p>{truncate(rental.description)}</p>
-				</div>
+				<h3>{rental.name}</h3>
+				<p>
+					<strong>ID:</strong> {rental.id}
+				</p>
+				<p>
+					<strong>Country:</strong> {rental.country}
+				</p>
+				<p>
+					<strong>City:</strong> {rental.city}
+					{rental.neighbourhood ? ' 📍 ' + rental.neighbourhood : null}
+				</p>
+				<p>{truncate(rental.description)}</p>
+			</div>
 
-				<div className="card_button">
-					<Button />
-					<button id="deleteItem" className="btn btn-primary mt-3" onClick={() => {
-						console.log("Delete button clicked")
-						clickToDelete(rental.id)}
-					}>
-						Delete <Trash />
-					</button>
-				</div>
+			<div className={styles.flatcard_footer}>
+				<Button
+					text="Delete"
+					type="secondary"
+					fullWidth
+					iconRight={<Trash />}
+					onClick={() => {
+						clickToDelete(rental.id)
+					}}
+				/>
 			</div>
 		</div>
 	)
