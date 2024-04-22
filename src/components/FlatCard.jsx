df2a148c56431c8e1e00d3ae5947a0febf8454b9
Laura Sinclair <l.sinclair@tuta.io>
2024-04-22 20:50:48 +0200

    adding colors for ratings

--- src/components/FlatCard.jsx
+++ src/components/FlatCard.jsx
@@ -1,7 +1,9 @@
-import { Trash, StarFill } from 'react-bootstrap-icons'
+import { Trash, StarFill, Pen } from 'react-bootstrap-icons'
 import placeholder from '@img/placeholder_image.jpg'
-import { Button } from '@components';
+import { Button } from '@components'
+import { Container, Row, Col } from 'react-bootstrap'
 import styles from './FlatCard.module.sass'
+import { useState, useEffect } from 'react'
 
 export default function FlatCard(props) {
 	const { rental, clickToDelete } = props
@@ -10,11 +12,25 @@ export default function FlatCard(props) {
 		return str && str.length > 100 ? str.substring(0, 100) + ' (...)' : str
 	}
 
+	const [ratingStyle, setRatingStyle] = useState('');
+
+	useEffect(() => {
+	  if (rental.review_scores_rating !== undefined) {
+		if (rental.review_scores_rating <= 50) {
+		  setRatingStyle(styles.flatcard_score_bad);
+		} else if (rental.review_scores_rating > 50 && rental.review_scores_rating < 75) {
+		  setRatingStyle(styles.flatcard_score_okay);
+		} else if (rental.review_scores_rating >= 75) {
+		  setRatingStyle(styles.flatcard_score_good);
+		}
+	  }
+	}, [rental.review_scores_rating]);
+
 	return (
 		<div className={styles.flatcard}>
 			<div className={styles.flatcard_thumbnail} style={{ backgroundImage: 'url(' + placeholder + ')' }}>
 				{rental.review_scores_rating && (
-					<p className={styles.flatcard_score}>
+					<p className={`${styles.flatcard_score} ${ratingStyle}`}>
 						<StarFill size="15" />
 						<span>{(rental.review_scores_rating / 20).toFixed(1)}</span>/5
 					</p>
@@ -36,15 +52,22 @@ export default function FlatCard(props) {
 			</div>
 
 			<div className={styles.flatcard_footer}>
-				<Button
-					text="Delete"
-					type="secondary"
-					fullWidth
-					iconRight={<Trash />}
-					onClick={() => {
-						clickToDelete(rental.id)
-					}}
-				/>
+				<Row>
+					<Col className="pe-md-1">
+						<Button text="Edit" type="primary" fullWidth iconRight={<Pen />} />
+					</Col>
+					<Col className="ps-md-1">
+						<Button
+							text="Delete"
+							type="secondary"
+							fullWidth
+							iconRight={<Trash />}
+							onClick={() => {
+								clickToDelete(rental.id)
+							}}
+						/>
+					</Col>
+				</Row>
 			</div>
 		</div>
 	)
