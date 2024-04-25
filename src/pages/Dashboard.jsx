95445c4a774bd9622f6ce1e94cdd16ef567685e2
Laura Sinclair <l.sinclair@tuta.io>
2024-04-25 16:20:00 +0200

    so close

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,13 +20,16 @@ export default function Dashboard({ loading }) {
 	const deleteRental = (rentalId) => {
 		const updatedRentals = rentals.filter(rental => rental.id !== rentalId);
 		setRentals(updatedRentals);
-		localStorage.setItem('rentals', JSON.stringify(updatedRentals));
+		localStorage.setItem('rentalsInLocalStorage', JSON.stringify(updatedRentals));
 		setReload(true)
 	}
 
 	const handleAddRental = (newRental) => {
-		rentals.push(newRental)
+		const updatedRentals = JSON.parse(localStorage.getItem('rentalsInLocalStorage'))
+		updatedRentals.push(newRental)
 		setRentals([newRental, ...rentals])
+		localStorage.setItem('rentalsInLocalStorage', JSON.stringify(updatedRentals));
+		setReload(true)
 	}
 
 	if (loading) return <div> loading... </div>
