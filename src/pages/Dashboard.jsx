0c4216ba5b90e89b8b5c3063b54cbc4bc4978672
Laura Sinclair <l.sinclair@tuta.io>
2024-05-10 14:17:31 +0200

    Update rentals path

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
 				setError("Couldn't fetch rentals")
 			}
 		} else {
-			fetch('/src/assets/data/rentals.json')
+			fetch('/src/assets/data/rentals.json' || '/data/rentals.json' )
 				.then((resp) => {
 					return resp.json()
 				})
