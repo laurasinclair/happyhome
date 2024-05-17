1be7be299052f00d4d4798f1fc927fb26e0d5a2c
Laura Sinclair <l.sinclair@tuta.io>
2024-05-18 00:12:26 +0200

    Making it a subdirectory, to be redirected from main domain

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/projects/happyhome">
       <App />
     </Router>
   </React.StrictMode>
