4de783029eb455c4c121e9818d6f54166babb925
Laura Sinclair <l.sinclair@tuta.io>
2024-06-09 03:03:56 +0200

    Rental page: edit/delete buttons have a 'cancel' option, and don't interfere with each other

--- src/components/states/Warning/index.jsx
+++ src/components/states/Warning/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './index.module.sass';
-import { X } from 'react-bootstrap-icons';
+import { ExclamationCircle } from 'react-bootstrap-icons';
 import classNames from 'classnames';
 
 function Warning({ children, className }) {
 	return (
 		<div className={classNames(styles.Warning, className)}>
-			<X size='32' />
+			<ExclamationCircle size='24' />
 			<p>
 				{children}
 			</p>
