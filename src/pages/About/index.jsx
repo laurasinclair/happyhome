f705b154150e3aedf2623709fba55cd1c4410ee4
Laura Sinclair <l.sinclair@tuta.io>
2024-06-10 22:32:48 +0200

    About page: took bullshit out of text

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -17,8 +17,16 @@ export default function About(props) {
 						md='10'
 						lg='8'
 						xl='6'>
-						<div>
-							<p className='mb-4'>
+						<div className='mb-4'>
+							<p className='lead'>
+								All done with my little hands, this dashboard was built and
+								designed to provide a user-friendly interface for managing
+								rental properties.
+							</p>
+						</div>
+
+						<div className='my-5'>
+							<p>
 								<a
 									href='https://github.com/laurasinclair/happyhome'
 									className='wavy d-block'
@@ -26,41 +34,23 @@ export default function About(props) {
 									GitHub repository
 								</a>
 							</p>
-
-							<p className='lead'>
-								All done with my little hands, this dashboard was built and
-								designed to provide a user-friendly interface for managing
-								rental properties.
-							</p>
-							<p>It includes features such as:</p>
-							<ul className='list-arrow'>
-								<li>Viewing rentals.</li>
-								<li>Adding, editing, and deleting information about rentals</li>
-								<li>Sorting/Filtering flats</li>
-							</ul>
 						</div>
-					</Col>
-				</Row>
 
-				<h2>Technologies used</h2>
-				<Row>
-					<Col md='6'>
+						<h2>Technologies used</h2>
 						<h3>Frontend</h3>
 						<ul className='list-arrow'>
 							<li>React.js</li>
 							<li>Vite </li>
 							<li>Axios </li>
 							<li>
-								Bootstrap - for grid system and layout helpers (margin,
-								padding). Everything else is custom ❤️
+								Bootstrap - for grid system and layout helpers. Everything else
+								is custom ❤️
 							</li>
 							<li>React Router (for client-side routing)</li>
 							<li>Node.js environment</li>
 							<li>Netlify (for frontend deployment)</li>
 						</ul>
-					</Col>
 
-					<Col md='6'>
 						<h3>Backend</h3>
 						<ul className='list-arrow'>
 							<li>Express.js</li>
